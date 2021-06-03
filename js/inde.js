43fa3f460ba0d7bc86c43409fcaf14a475400112
sebarray <sebarray@gmail.com>
2021-06-02 21:56:42 -0300

    v1

---
+++ js/inde.js
@@ -0,0 +1,14 @@
+
+edadverif();
+
+
+function edadverif( ){
+let edad=parseInt(prompt("cual es tu edad"));
+if( edad > 18){
+    alert("bievenid@")
+}
+else{
+    alert("para comprar asegurate de estar con un mayor")
+}
+
+}
