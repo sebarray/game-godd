4d88b65612023ca7e89d1b8b61560435adb1d40e
sebarray <sebarray@gmail.com>
2021-06-09 01:39:40 -0300

    trabajo con arrays

--- docs/js/inde.js
+++ docs/js/inde.js
@@ -1,14 +1,66 @@
+class game {
+    constructor() {}
+    create() {
+        this.nombre = prompt(" ingresar nombre")
+        this.genero = prompt("ingresar genero").trim()
+        this.precio = parseFloat(prompt("ingresar precio"))
 
+    }
+}
+
+
+
+function edadverif() {
+    let edad = parseInt(prompt("cual es tu edad"));
+    if (edad > 182) {
+        alert("bievenid@")
+    } else {
+        alert("para comprar asegurate de estar con un mayor")
+    }
+
+}
+
+
+
+
+let i
+let contrador
+const listaJuegos = []
+const juegos = new game()
 edadverif();
+contrador = prompt("cuanto juegos desea ingresar ")
 
 
-function edadverif( ){
-let edad=parseInt(prompt("cual es tu edad"));
-if( edad > 182){
-    alert("bievenid@")
+for (i = 0; i < contrador; i++) {
+    do{
+    juegos.create()
+    listaJuegos[i] = juegos
+    listaJuegos[i].genero.trim()
+    }while( generoFiltro(listaJuegos[i].genero))
+    console.log(listaJuegos[i])
+    alert(listaJuegos[i])
 }
-else{
-    alert("para comprar asegurate de estar con un mayor")
-}
+
+
+
+
+function generoFiltro(genero){
+    switch(genero){
+        case"accion":{
+            return false
+        }
+        case"aventura":{
+            return false
+
+        }
+        case"deporte":{
+            return false
+        }
+        default:{
+            console.log("el genero ingresado no es valido")
+            return true
+        }
+
+    }
 
 }
