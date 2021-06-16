fa2835d26c3e46717b5ec8643b1f16dfccce1a22
sebarray <54658214+sebarray@users.noreply.github.com>
2021-06-16 18:54:29 -0300

    Update inde.js

--- docs/js/inde.js
+++ docs/js/inde.js
@@ -19,22 +19,6 @@ function edadverif() {
     }
 
 }
-function createLista(i, contrador,  listaJuegos, juegos ){
-
-    for (i = 0; i < contrador; i++) {
-        do{
-        juegos.create()
-        listaJuegos.push({
-            nombre:juegos.nombre,
-            genero:juegos.genero,
-            precio:juegos.precio
-        })
-        listaJuegos[i].genero.trim()
-        }while( generoFiltro(listaJuegos[i].genero))
-    }
-    return listaJuegos
-} 
-
 
 
 
@@ -61,6 +45,37 @@ function generoFiltro(genero){
 
 
 
+
+
+
+
+
+
+function createLista(i, contrador,  listaJuegos, juegos ){
+
+    for (i = 0; i < contrador; i++) {
+        do{
+        juegos.create()
+        listaJuegos.push({
+            nombre:juegos.nombre,
+            genero:juegos.genero,
+            precio:juegos.precio
+        })
+        listaJuegos[i].genero.trim()
+        }while( generoFiltro(listaJuegos[i].genero))
+    }
+    return listaJuegos
+} 
+
+
+
+
+/*for(nom of nombres){
+    console.log(nom.innerHTML)   
+   }   
+const nombres = document.getElementsByClassName('card-title')
+console.log(nombres)
+*/
 let i
 let contrador
 const listaJuegos = []
@@ -78,7 +93,16 @@ lista.sort((a,b) =>{
  console.log(lista);
 
 
-
+ for (const producto of lista) {
+    let contenedor = document.createElement("div");
+    //Definimos el innerHTML del elemento con una plantilla de texto
+    contenedor.innerHTML = `<div class="container bg-dark rounded col-lg-12 text-light">
+                            <h3> genero: ${producto.genero}</h3>
+                            <p>  Producto: ${producto.nombre}</p>
+                            <b> $ ${producto.precio}</b>
+                            </div>`;
+    document.body.appendChild(contenedor);
+}
 
 
 
